d5f179695e428d4eaaf3d1642b0b5145c650ae42
Yusuke0507 <xitianxiongjie0@gmail.com>
2021-08-26 11:10:51 +0900

    予約情報保存機能

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -7,4 +7,15 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.new
     @menu = Menu.all
   end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.save
+  end
+
+  private
+  def reservation_params
+    params.require(:reservation).permit(:time, :date, :people, :remark, :menu_id).merge(user_id: current_user.id)
+  end
 end
+

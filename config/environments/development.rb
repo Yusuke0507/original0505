903d38ff914005c9d4aa4877ef6c4b014c4acff0
Yusuke0507 <xitianxiongjie0@gmail.com>
2021-09-06 17:51:39 +0900

    :only_path true を追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,13 +61,17 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   Rails.application.configure do
     # default url
-    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+    config.action_mailer.default_url_options = {
+       protocol: 'https',
+       host: ENV["CLOUD9_APP_ROOT_URL"]
+     }
+    config.action_mailer.default_url_options = { host: 'herokuapp.com', port: 3000 }
     # mail setting
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
-      :port => 587,
+      :port => 587, 
       :user_name => ENV["GMAIL_ADDRESS"],
       :password => ENV["GMAIL_2FACTOR_PASSWORD"],
       :authentication => :plain,

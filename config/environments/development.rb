a4fb406dc7e441f86e113ec867c659d8e195e8bc
Yusuke0507 <xitianxiongjie0@gmail.com>
2021-09-07 12:44:53 +0900

    元に戻す

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,8 +61,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   Rails.application.configure do
     # default url
-    config.action_mailer.default_url_options = { host: host }
-    host =  'https://original0505.herokuapp.com/'
+    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
     # mail setting
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp

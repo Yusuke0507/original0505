27ba7481ebabb7b36e7cdbbb73b010e0fb7aa69d
Yusuke0507 <xitianxiongjie0@gmail.com>
2021-09-07 13:51:01 +0900

    メール設定のみ記述

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,10 +109,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  Rails.application.configure do
-    # default url
-    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
     # mail setting
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
